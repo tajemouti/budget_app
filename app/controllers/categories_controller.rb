b02196ce5b45fd4c8fcab0b0eec4571fb325001b
tajemouti <tajemouti@gmail.com>
2023-11-28 10:47:07 +0100

    Implement categories index and new views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,30 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = current_user.categories
   end
 
   def show
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+    @category.author = current_user
+
+    if @category.save
+      flash[:notice] = 'Category successfully created.'
+      redirect_to categories_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
   end
 end
