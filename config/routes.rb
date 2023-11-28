e62c2d6e1e6b1651cd0d8de2767a0d9dfe89ddf1
tajemouti <tajemouti@gmail.com>
2023-11-28 09:32:24 +0100

    Generate Categories controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'categories/index'
+  get 'categories/show'
+  get 'categories/new'
+  get 'categories/create'
   get 'splash/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
