436c3d1e1afc6c4029d4d37cda958750358da2c6
tajemouti <tajemouti@gmail.com>
2023-11-28 01:26:11 +0100

    Add validations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :categories, dependent: :destroy, foreign_key: :author_id
   has_many :payments, dependent: :destroy, foreign_key: :author_id
+
+  validates :name, presence: true
 end
